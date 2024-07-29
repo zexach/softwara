814bf14f35196ff77e2dfd9f28c7d97f04df7a05
Emir Zambakovic <83666062+zexach@users.noreply.github.com>
2024-07-29 15:46:23 +0200

    Added logic to Textarea component

--- src/components/Input/TextArea/TextArea.tsx
+++ src/components/Input/TextArea/TextArea.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './TextArea.scss';
 
 type Props = {
@@ -9,17 +9,27 @@ type Props = {
 
 const TextArea: React.FC<Props> = ({ placeholder, isRequired }) => {
 
+    const [isEmpty, setIsEmpty] = useState<boolean>(false);
+    const [inputText, setInputText] = useState<string>('');
+
+    useEffect(() => {
+        console.log(isEmpty);
+    }, [isEmpty])
+
     return(
         <>
         <div className="text-area">
             <label className="text-area__label">Your message</label>
             <textarea
+                onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => setInputText(e.target.value)}
+                onBlur={() => { inputText !== '' ? setIsEmpty(false) : setIsEmpty(true) }}
                 rows={10}
                 cols={100}
                 name="message"
                 placeholder={placeholder}
                 id="message"
                 className="text-area__input"></textarea>
+            { (isRequired && isEmpty) ? <p className="text-area__warning">This field is required</p> : '' }
         </div>
         </>
     );
