8601f25d747ffe6006d582871805280632e2c0df
Emir Zambakovic <83666062+zexach@users.noreply.github.com>
2024-08-09 17:00:13 +0200

    Performance optimization

--- src/pages/ContactPage/ContactPage.tsx
+++ src/pages/ContactPage/ContactPage.tsx
@@ -17,7 +17,7 @@ const ContactPage: React.FC = () => {
                     viewport={{ once: true }}
                     className="contact-page__content">
                     <div className="contact-page__content__info">
-                        <h3 className="contact-page__content__info__header">Get in Touch</h3>
+                        <h1 className="contact-page__content__info__header">Get in Touch</h1>
                         <p className="contact-page__content__info__desc">Whether you're interested in working together or just want to say hello, send us a message below.</p>
                     </div>
                     <ContactForm onSubmitMail={sendEmail} />
